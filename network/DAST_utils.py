8349008a80a9fba5b963546a8a3d1fd36847a0d2
qawl987 <shiny861216@gmail.com>
2024-02-12 19:16:49 +0800

    feat: change positional encoding odd part

--- network/DAST_utils.py
+++ network/DAST_utils.py
@@ -168,19 +168,15 @@ class PositionalEncoding(nn.Module):
         super(PositionalEncoding, self).__init__()
         pe = torch.zeros(max_len, d_model)
         position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
-        
-        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
-        
-        pe[:, 0::2] = torch.sin(position * div_term)
-        pe[:, 1::2] = torch.cos(position * div_term)
-        
+        div_term_even = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+        div_term_odd = torch.exp(torch.arange(1, d_model, 2).float() * (-math.log(10000.0) / d_model))
+        pe[:, 0::2] = torch.sin(position * div_term_even)
+        pe[:, 1::2] = torch.cos(position * div_term_odd)
         pe = pe.unsqueeze(0).transpose(0, 1)
-        
         self.register_buffer('pe', pe)
 
     def forward(self, x):
-        x = x + self.pe[:x.size(1), :]. squeeze(1)
-        #print(x.size())
+        x = x + self.pe[:x.size(1), :].squeeze(1)
         return x     
 
 
