6ad870511a3d12edbdcebce81dda38b67e5a4afd
qawl987 <shiny861216@gmail.com>
2024-02-16 19:17:21 +0800

    feat: multi-scale encoder

--- network/DAST_Network.py
+++ network/DAST_Network.py
@@ -4,50 +4,55 @@ import torch.nn.functional as F
 import numpy as np
 from DAST_utils import *
 import sys
-DEBUG = True
+DEBUG = False
+
 
 class Sensors_EncoderLayer(torch.nn.Module):
-    def __init__(self, dim_val, dim_attn, n_heads = 1,dropout=0.1):
+    def __init__(self, dim_val, dim_attn, n_heads=1, dropout=0.1):
         super(Sensors_EncoderLayer, self).__init__()
-        self.attn = Sensor_MultiHeadAttentionBlock(dim_val, dim_attn , n_heads)
+        self.attn = Sensor_MultiHeadAttentionBlock(dim_val, dim_attn, n_heads)
         self.fc1 = nn.Linear(dim_val, dim_val)
-        self.fc2 = nn.Linear(dim_val, dim_val)   
+        self.fc2 = nn.Linear(dim_val, dim_val)
         self.norm1 = nn.LayerNorm(dim_val)
         self.norm2 = nn.LayerNorm(dim_val)
         self.dropout = nn.Dropout(dropout)
-    
+
     def forward(self, x):
-        if DEBUG == True: print('Sensor encoder input: ', x.shape)
+        if DEBUG == True:
+            print('Sensor encoder input: ', x.shape)
         a = self.attn(x)
-        if DEBUG == True: print('Sensor encoder attn: ', a.size())
-        x = self.norm1(x + a)  
+        if DEBUG == True:
+            print('Sensor encoder attn: ', a.size())
+        x = self.norm1(x + a)
         a = self.fc1(F.elu(self.fc2(x)))
-        x = self.norm2(x + a)        
-        return x  
+        x = self.norm2(x + a)
+        return x
 
 
 class Time_step_EncoderLayer(torch.nn.Module):
-    def __init__(self, dim_val, dim_attn, n_heads = 1,dropout=0.1):
+    def __init__(self, dim_val, dim_attn, n_heads=1, dropout=0.1):
         super(Time_step_EncoderLayer, self).__init__()
-        self.attn = TimeStepMultiHeadAttentionBlock(dim_val, dim_attn , n_heads)
+        self.attn = TimeStepMultiHeadAttentionBlock(dim_val, dim_attn, n_heads)
         self.fc1 = nn.Linear(dim_val, dim_val)
         self.fc2 = nn.Linear(dim_val, dim_val)
         self.dropout = nn.Dropout(dropout)
         self.norm1 = nn.LayerNorm(dim_val)
         self.norm2 = nn.LayerNorm(dim_val)
-    
+
     def forward(self, x):
-        if DEBUG == True: print('Time encoder input: ', x.size())
+        if DEBUG == True:
+            print('Time encoder input: ', x.size())
         a = self.attn(x)
-        if DEBUG == True: print('Time encoder attn: ', a.size())
-        x = self.norm1(x + a)                 
-        a = self.fc1(F.elu(self.fc2(x))) 
-        x = self.norm2(x + a)          
-        return x  
+        if DEBUG == True:
+            print('Time encoder attn: ', a.size())
+        x = self.norm1(x + a)
+        a = self.fc1(F.elu(self.fc2(x)))
+        x = self.norm2(x + a)
+        return x
 
 
 class DecoderLayer(torch.nn.Module):
-    def __init__(self, dim_val, dim_attn, n_heads = 1,dropout=0.1):
+    def __init__(self, dim_val, dim_attn, n_heads=1, dropout=0.1):
         super(DecoderLayer, self).__init__()
         self.attn1 = MultiHeadAttentionBlock(dim_val, dim_attn, n_heads)
         self.attn2 = MultiHeadAttentionBlock(dim_val, dim_attn, n_heads)
@@ -57,84 +62,129 @@ class DecoderLayer(torch.nn.Module):
         self.norm1 = nn.LayerNorm(dim_val)
         self.norm2 = nn.LayerNorm(dim_val)
         self.norm3 = nn.LayerNorm(dim_val)
-        
+
     def forward(self, x, enc):
-        a = self.attn1(x) 
-        x = self.norm1(a + x)        
-        a = self.attn2(x, kv = enc)
+        a = self.attn1(x)
+        x = self.norm1(a + x)
+        a = self.attn2(x, kv=enc)
         x = self.norm2(a + x)
-        a = self.fc1(F.elu(self.fc2(x)))       
+        a = self.fc1(F.elu(self.fc2(x)))
         x = self.norm3(x + a)
-        return x  
+        return x
+
 
 class DAST(torch.nn.Module):
-    def __init__(self, dim_val_s, dim_attn_s, dim_val_t,dim_attn_t,dim_val, dim_attn, time_step, 
-                 input_size, dec_seq_len, out_seq_len, n_decoder_layers = 1, n_encoder_layers = 1,
-                 n_heads = 1, debug = False, dropout = 0.1):
-        
+    def __init__(self, dim_val_s, dim_attn_s, dim_val_t, dim_attn_t, dim_val, dim_attn, time_step,
+                 input_size, dec_seq_len, out_seq_len, n_decoder_layers=1, n_encoder_layers=1,
+                 n_heads=1, feature_len=16, debug=False, dropout=0.1):
+
         super(DAST, self).__init__()
         self.dec_seq_len = dec_seq_len
         self.dropout = nn.Dropout(dropout)
         self.debug = debug
-        #Initiate Sensors encoder
-        self.sensor_encoder = nn.ModuleList() # []
+        # Initiate Sensors encoder
+        self.sensor_encoder1 = nn.ModuleList()  # []
+        self.sensor_encoder2 = nn.ModuleList()
+        self.sensor_encoder3 = nn.ModuleList()
         for i in range(n_encoder_layers):
-            self.sensor_encoder.append(Sensors_EncoderLayer(dim_val_s, dim_attn_s, n_heads))
-                
-        #Initiate Time_step encoder
+            self.sensor_encoder1.append(
+                Sensors_EncoderLayer(dim_val_s, dim_attn_s, n_heads))
+            self.sensor_encoder2.append(
+                Sensors_EncoderLayer(dim_val_s, dim_attn_s, n_heads))
+            self.sensor_encoder3.append(
+                Sensors_EncoderLayer(dim_val_s, dim_attn_s, n_heads))
+        # Initiate Time_step encoder
         self.time_encoder = nn.ModuleList()   # []
         for i in range(n_encoder_layers):
-            self.time_encoder.append(Time_step_EncoderLayer(dim_val_t, dim_attn_t, n_heads))        
-                
-        #Initiate Decoder
-        self.decoder = nn.ModuleList()
+            self.time_encoder.append(Time_step_EncoderLayer(
+                dim_val_t, dim_attn_t, n_heads))
+
+        # Initiate Decoder
+        self.decoder1 = nn.ModuleList()
+        self.decoder2 = nn.ModuleList()
+        self.decoder3 = nn.ModuleList()
         for i in range(n_decoder_layers):
-            self.decoder.append(DecoderLayer(dim_val, dim_attn, n_heads))
-                    
-        self.pos_s = PositionalEncoding(dim_val_s)  
-        self.pos_t = PositionalEncoding(dim_val_t)   
+            self.decoder1.append(DecoderLayer(dim_val, dim_attn, n_heads))
+            self.decoder2.append(DecoderLayer(dim_val, dim_attn, n_heads))
+            self.decoder3.append(DecoderLayer(dim_val, dim_attn, n_heads))
+
+        self.pos_s = PositionalEncoding(dim_val_s)
+        self.pos_t = PositionalEncoding(dim_val_t)
         self.timestep_enc_input_fc = nn.Linear(input_size, dim_val_t)
-        self.sensor_enc_input_fc = nn.Linear(time_step, dim_val_s)        
-        self.dec_input_fc = nn.Linear(input_size, dim_val)
-        self.out_fc = nn.Linear(dec_seq_len * dim_val, out_seq_len)
+        self.sensor_enc_input_fc = nn.Linear(time_step, dim_val_s)
+        self.dec_input_fc1 = nn.Linear(input_size, dim_val)
+        self.dec_input_fc2 = nn.Linear(input_size * 2, dim_val)
+        self.dec_input_fc3 = nn.Linear(input_size * 4, dim_val)
+        self.out_fc = nn.Linear(dec_seq_len * dim_val * 3, out_seq_len)
         self.norm1 = nn.LayerNorm(dim_val)
-        
-    
+        self.feature_len = feature_len
+
     def forward(self, x):
-        #input embedding and positional encoding
-        if self.debug == True: print('input X: ', x.size())
-        sensor_x = x.transpose(1,2)
-        if self.debug == True: print('sensor X: ', sensor_x.size())
-        print(self.sensor_enc_input_fc(sensor_x).shape)
-        e = self.sensor_encoder[0](self.sensor_enc_input_fc(sensor_x)) #((batch_size,sensor,dim_val_s))
-        if self.debug == True: print('sensor encoder X: ', e.size())
-        print(self.timestep_enc_input_fc(x).shape)
-        o = self.time_encoder[0](self.pos_t(self.timestep_enc_input_fc(x))) #((batch_size,timestep,dim_val_t))
-        if self.debug == True: print('time encoder X: ', o.size())
-        # sensors encoder 
-        for sensor_enc in self.sensor_encoder[1:]:
-            e = sensor_enc(e)
+        sensor_x = x.transpose(1, 2)
+        feature1 = x[:, :, :self.feature_len].transpose(1, 2)
+        feature2 = x[:, :, self.feature_len: 3 *
+                     self.feature_len].transpose(1, 2)
+        feature3 = x[:, :, 3 * self.feature_len: 7 *
+                     self.feature_len].transpose(1, 2)
+        x = x[:, :, :self.feature_len]
+        # input embedding and positional encoding
+        if DEBUG == True:
+            print('x shape:', x.shape)
+            print('input X: ', x.size())
+            print('sensor X: ', sensor_x.size())
+            print(feature1.shape, feature2.shape, feature3.shape, sep='\n')
+            print(self.sensor_enc_input_fc(feature1).shape)
+            print(self.sensor_enc_input_fc(feature2).shape)
+            print(self.sensor_enc_input_fc(feature3).shape)
         
-        #time step encoder 
+        e1 = self.sensor_encoder1[0](self.sensor_enc_input_fc(
+            feature1))  # ((batch_size,sensor,dim_val_s))
+        e2 = self.sensor_encoder1[0](self.sensor_enc_input_fc(
+            feature2))
+        e3 = self.sensor_encoder1[0](self.sensor_enc_input_fc(
+            feature3))
+        if DEBUG == True:
+            print('sensor encoder X: ', e1.shape, e2.shape, e3.shape, )
+            print(self.timestep_enc_input_fc(x).shape)
+        # ((batch_size,timestep,dim_val_t))
+        o = self.time_encoder[0](self.pos_t(self.timestep_enc_input_fc(x)))
+        if DEBUG == True:
+            print('time encoder X: ', o.size())
+        # sensors encoder
+        for sensor_enc in self.sensor_encoder1[1:]:
+            e1 = sensor_enc(e1)
+        for sensor_enc in self.sensor_encoder2[1:]:
+            e2 = sensor_enc(e2)
+        for sensor_enc in self.sensor_encoder3[1:]:
+            e3 = sensor_enc(e3)
+        # time step encoder
         for time_enc in self.time_encoder[1:]:
             o = time_enc(o)
-            
-        #feature fusion
-        p = torch.cat((e,o), dim = 1)  #((batch_size,timestep+sensor,dim_val))
-        p = self.norm1(p)               
+
+        # feature fusion
+        p1 = torch.cat((e1, o), dim=1)  # ((batch_size,timestep+sensor,dim_val))
+        p1 = self.norm1(p1)
+
+        p2 = torch.cat((e2, o), dim=1)
+        p2 = self.norm1(p2)
         
-        #decoder receive the output of feature fusion layer.        
-        if self.debug == True: print('decoder X: ', p.size())
-        d = self.decoder[0](self.dec_input_fc(x[:,-self.dec_seq_len:]), p) 
-        if self.debug == True: print('embedding decoder X: ', d.size())
-        #output the RUL
-        #x = self.out_fc(d.flatten(start_dim=1))
-        x = self.out_fc(F.elu(d.flatten(start_dim=1)))   
-        if self.debug == True: print('output X: ', x.size())   
+        p3 = torch.cat((e3, o), dim=1)
+        p3 = self.norm1(p3)
+        # decoder receive the output of feature fusion layer.
+        if DEBUG == True:
+            print('decoder X: ', p1.size())
+            print('f1 decode no transpose: ', feature1[:, -self.dec_seq_len:].shape)
+            print('f1 decode: ', feature1.transpose(1, 2)[:, -self.dec_seq_len:].shape)
+        d1 = self.decoder1[0](self.dec_input_fc1(feature1.transpose(1, 2)[:, -self.dec_seq_len:]), p1)
+        d2 = self.decoder2[0](self.dec_input_fc2(feature2.transpose(1, 2)[:, -self.dec_seq_len:]), p2)
+        d3 = self.decoder3[0](self.dec_input_fc3(feature3.transpose(1, 2)[:, -self.dec_seq_len:]), p3)
+        if DEBUG == True:
+            print('embedding decoder X: ', d1.size())
+        # output the RUL
+        d = torch.cat((d1, d2, d3), dim=2)
+        x = self.out_fc(F.elu(d.flatten(start_dim=1)))
+        if DEBUG == True:
+            print('x: ', x.shape)
+            print('d1+d2+d3: ', d.shape)
+            print('output X: ', x.size())
         return x
-    
-
-
-    
-    
-   
--- network/DAST_utils.py
+++ network/DAST_utils.py
@@ -4,175 +4,194 @@ import torch.nn.functional as F
 import numpy as np
 import math
 from enum import Enum
-DEBUG = True
+DEBUG = False
+
 
 def a_norm(Q, K):
-    m = torch.matmul(Q, K.transpose(2,1).float())
-    m /= torch.sqrt(torch.tensor(Q.shape[-1]).float())    
-    return torch.softmax(m,-1)
+    m = torch.matmul(Q, K.transpose(2, 1).float())
+    m /= torch.sqrt(torch.tensor(Q.shape[-1]).float())
+    return torch.softmax(m, -1)
+
 
 def Sensor_a_norm(Q, K):
-    m = torch.matmul(Q, K.transpose(2,1).float())
-    m /= torch.sqrt(torch.tensor(Q.shape[-1]).float())    
-    return torch.softmax(m,-1)
+    m = torch.matmul(Q, K.transpose(2, 1).float())
+    m /= torch.sqrt(torch.tensor(Q.shape[-1]).float())
+    return torch.softmax(m, -1)
+
 
 def time_step_a_norm(Q, K):
-    m = torch.matmul(Q, K.transpose(2,1).float())
-    m /= torch.sqrt(torch.tensor(Q.shape[-1]).float())    
-    return torch.softmax(m,-1)
+    m = torch.matmul(Q, K.transpose(2, 1).float())
+    m /= torch.sqrt(torch.tensor(Q.shape[-1]).float())
+    return torch.softmax(m, -1)
 
-#attention
+# attention
+
 
 def attention(Q, K, V):
-    a = a_norm(Q, K) 
-    return  torch.matmul(a,  V)
+    a = a_norm(Q, K)
+    return torch.matmul(a,  V)
+
 
 def Sensor_attention(Q, K, V):
-    a = Sensor_a_norm(Q, K) 
-    return  torch.matmul(a,  V) 
+    a = Sensor_a_norm(Q, K)
+    return torch.matmul(a,  V)
+
 
 def time_step_attention(Q, K, V):
-    a = time_step_a_norm(Q, K)  
-    return  torch.matmul(a,  V)
+    a = time_step_a_norm(Q, K)
+    return torch.matmul(a,  V)
 
-#AttentionBlock
+# AttentionBlock
+
+
 class AttentionBlock(torch.nn.Module):
     def __init__(self, dim_val, dim_attn):
         super(AttentionBlock, self).__init__()
-        self.value = Value(dim_val, dim_val)  
-        self.key = Key(dim_val, dim_attn)  
-        self.query = Query(dim_val, dim_attn) 
-    
-    def forward(self, x, kv = None): 
-        if(kv is None):
-            return attention(self.query(x), self.key(x), self.value(x))        
+        self.value = Value(dim_val, dim_val)
+        self.key = Key(dim_val, dim_attn)
+        self.query = Query(dim_val, dim_attn)
+
+    def forward(self, x, kv=None):
+        if (kv is None):
+            return attention(self.query(x), self.key(x), self.value(x))
         return attention(self.query(x), self.key(kv), self.value(kv))
+
 
 class Sensor_AttentionBlock(torch.nn.Module):
     def __init__(self, dim_val, dim_attn):
         super(Sensor_AttentionBlock, self).__init__()
-        self.value = Value(dim_val, dim_val) 
-        self.key = Key(dim_val, dim_attn) 
+        self.value = Value(dim_val, dim_val)
+        self.key = Key(dim_val, dim_attn)
         self.query = Query(dim_val, dim_attn)
-    
-    def forward(self, x, kv = None): 
-        if(kv is None):
-            return Sensor_attention(self.query(x), self.key(x), self.value(x))        
+
+    def forward(self, x, kv=None):
+        if (kv is None):
+            return Sensor_attention(self.query(x), self.key(x), self.value(x))
         return Sensor_attention(self.query(x), self.key(kv), self.value(kv))
-    
+
+
 class time_step_AttentionBlock(torch.nn.Module):
     def __init__(self, dim_val, dim_attn):
         super(time_step_AttentionBlock, self).__init__()
-        self.value = Value(dim_val, dim_val) 
-        self.key = Key(dim_val, dim_attn)  
-        self.query = Query(dim_val, dim_attn) 
-    
-    def forward(self, x, kv = None): 
-        if(kv is None):
-            return time_step_attention(self.query(x), self.key(x), self.value(x))        
+        self.value = Value(dim_val, dim_val)
+        self.key = Key(dim_val, dim_attn)
+        self.query = Query(dim_val, dim_attn)
+
+    def forward(self, x, kv=None):
+        if (kv is None):
+            return time_step_attention(self.query(x), self.key(x), self.value(x))
         return time_step_attention(self.query(x), self.key(kv), self.value(kv))
 
 
-
-# Multi-head self-attention 
-class MultiHeadAttentionBlock(torch.nn.Module):  
-    def __init__(self, dim_val, dim_attn, n_heads): 
+# Multi-head self-attention
+class MultiHeadAttentionBlock(torch.nn.Module):
+    def __init__(self, dim_val, dim_attn, n_heads):
         super(MultiHeadAttentionBlock, self).__init__()
         self.heads = []
         for i in range(n_heads):
-            self.heads.append(AttentionBlock(dim_val,  dim_attn))       
+            self.heads.append(AttentionBlock(dim_val,  dim_attn))
         self.heads = nn.ModuleList(self.heads)
-        self.fc = nn.Linear(n_heads * dim_val, dim_val, bias = False)
-                      
-        
-    def forward(self, x, kv = None):
+        self.fc = nn.Linear(n_heads * dim_val, dim_val, bias=False)
+
+    def forward(self, x, kv=None):
         a = []
         for h in self.heads:
-            a.append(h(x, kv = kv))           
-        a = torch.stack(a, dim = -1) 
-        a = a.flatten(start_dim = 2)        
-        x = self.fc(a) 
+            a.append(h(x, kv=kv))
+        a = torch.stack(a, dim=-1)
+        a = a.flatten(start_dim=2)
+        x = self.fc(a)
         return x
 
-class Sensor_MultiHeadAttentionBlock(torch.nn.Module): 
-    def __init__(self, dim_val, dim_attn, n_heads): 
+
+class Sensor_MultiHeadAttentionBlock(torch.nn.Module):
+    def __init__(self, dim_val, dim_attn, n_heads):
         super(Sensor_MultiHeadAttentionBlock, self).__init__()
         self.heads = []
         for i in range(n_heads):
-            self.heads.append(Sensor_AttentionBlock(dim_val,  dim_attn))       
-        self.heads = nn.ModuleList(self.heads)      
-        self.fc = nn.Linear(n_heads * dim_val, dim_val, bias = False)
-                              
-    def forward(self, x, kv = None):
+            self.heads.append(Sensor_AttentionBlock(dim_val,  dim_attn))
+        self.heads = nn.ModuleList(self.heads)
+        self.fc = nn.Linear(n_heads * dim_val, dim_val, bias=False)
+
+    def forward(self, x, kv=None):
         a = []
-        if DEBUG == True: print('Sensor MHA input: ', x.shape)
+        if DEBUG == True:
+            print('Sensor MHA input: ', x.shape)
         for h in self.heads:
-            a.append(h(x, kv = kv))
-        a = torch.stack(a, dim = -1) 
-        a = a.flatten(start_dim = 2)         
-        x = self.fc(a) 
-        if DEBUG == True: print('Sensor MHA output: ', x.shape)
+            a.append(h(x, kv=kv))
+        a = torch.stack(a, dim=-1)
+        a = a.flatten(start_dim=2)
+        x = self.fc(a)
+        if DEBUG == True:
+            print('Sensor MHA output: ', x.shape)
         return x
 
-class TimeStepMultiHeadAttentionBlock(torch.nn.Module): 
-    def __init__(self, dim_val, dim_attn, n_heads): 
+
+class TimeStepMultiHeadAttentionBlock(torch.nn.Module):
+    def __init__(self, dim_val, dim_attn, n_heads):
         super(TimeStepMultiHeadAttentionBlock, self).__init__()
         self.heads = []
         for i in range(n_heads):
-            self.heads.append(time_step_AttentionBlock(dim_val,  dim_attn))        
-        self.heads = nn.ModuleList(self.heads)        
-        self.fc = nn.Linear(n_heads * dim_val, dim_val, bias = False)
-                              
-    def forward(self, x, kv = None):
+            self.heads.append(time_step_AttentionBlock(dim_val,  dim_attn))
+        self.heads = nn.ModuleList(self.heads)
+        self.fc = nn.Linear(n_heads * dim_val, dim_val, bias=False)
+
+    def forward(self, x, kv=None):
         a = []
         for h in self.heads:
-            a.append(h(x, kv = kv))                    
-        a = torch.stack(a, dim = -1) 
-        a = a.flatten(start_dim = 2)         
-        x = self.fc(a) 
+            a.append(h(x, kv=kv))
+        a = torch.stack(a, dim=-1)
+        a = a.flatten(start_dim=2)
+        x = self.fc(a)
         return x
 
 # Query, Key and Value
+
+
 class Value(torch.nn.Module):
     def __init__(self, dim_input, dim_val):
         super(Value, self).__init__()
-        self.dim_val = dim_val        
-        self.fc1 = nn.Linear(dim_input, dim_val, bias = False)
-    
+        self.dim_val = dim_val
+        self.fc1 = nn.Linear(dim_input, dim_val, bias=False)
+
     def forward(self, x):
-        x = self.fc1(x)       
+        x = self.fc1(x)
         return x
+
 
 class Key(torch.nn.Module):
     def __init__(self, dim_input, dim_attn):
         super(Key, self).__init__()
         self.dim_attn = dim_attn
-        self.fc1 = nn.Linear(dim_input, dim_attn, bias = False)
-    
+        self.fc1 = nn.Linear(dim_input, dim_attn, bias=False)
+
     def forward(self, x):
-        x = self.fc1(x)        
+        x = self.fc1(x)
         return x
+
 
 class Query(torch.nn.Module):
     def __init__(self, dim_input, dim_attn):
         super(Query, self).__init__()
-        self.dim_attn = dim_attn        
-        self.fc1 = nn.Linear(dim_input, dim_attn, bias = False)
+        self.dim_attn = dim_attn
+        self.fc1 = nn.Linear(dim_input, dim_attn, bias=False)
         # self.register_buffer('fc1', nn.Linear(dim_input, dim_attn, bias = False))
+
     def forward(self, x):
         # print(next(self.fc1.parameters()).device)
         x = self.fc1(x)
         return x
 
 
-#PositionalEncoding (from Transformer)
+# PositionalEncoding (from Transformer)
 class PositionalEncoding(nn.Module):
     def __init__(self, d_model, dropout=0.1, max_len=512):
         super(PositionalEncoding, self).__init__()
         pe = torch.zeros(max_len, d_model)
         position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
-        div_term_even = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
-        div_term_odd = torch.exp(torch.arange(1, d_model, 2).float() * (-math.log(10000.0) / d_model))
+        div_term_even = torch.exp(torch.arange(
+            0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+        div_term_odd = torch.exp(torch.arange(
+            1, d_model, 2).float() * (-math.log(10000.0) / d_model))
         pe[:, 0::2] = torch.sin(position * div_term_even)
         pe[:, 1::2] = torch.cos(position * div_term_odd)
         pe = pe.unsqueeze(0).transpose(0, 1)
@@ -180,24 +199,4 @@ class PositionalEncoding(nn.Module):
 
     def forward(self, x):
         x = x + self.pe[:x.size(1), :].squeeze(1)
-        return x     
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- 
+        return x
